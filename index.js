22438c7722d4836c5d218dc6661bc1ad26485aac
Jobel Iype <72402429+jobel1@users.noreply.github.com>
2024-12-24 12:06:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,30 +8,30 @@ async function start() {
     await page.goto("https://learnwebcode.github.io/practice-requests/")
     //await page.screenshot({path: "screenshot.png",fullPage:true})    //taking a screenshot of the page
     const names= await page.evaluate(() =>{    //saving html elements to text file
-     return Array.from(document.querySelectorAll(".info strong")).map(x => x.textContent) //return is used because the code is to be returned in node js
+     return Array.from(document.querySelectorAll(".info strong")).map(x => x.textContent) 
     })
     await fs.writeFile("names.txt", names.join("\r\n"))
-     //showing text that is behind the button. This code is written above photo looping code is that below code is searching for the image and it will show error when written below
+    ten below
 
-     await page.click("#clickme") //click is puppeteer method this code simulates click event
+     await page.click("#clickme") 
      const clickedData = await page.$eval("#data", a => a.textContent)
      console.log(clickedData)
     
-    //writefile is method inside fs  module.\r is return and \n is new line
-    const photos=await page.$$eval ("img",(imgs) => {   //"img" is css selector first argument and second argument is a function
-        return imgs.map(x => x.src)    //map function finds collection of data in "img" to function imgs
-      //$$eval() is used for selecting mutiple elements
+   
+    const photos=await page.$$eval ("img",(imgs) => {   
+        return imgs.map(x => x.src)    
+      
     })
 
   
-    await page.type("#ourfield", "blue") // has 2 arguments one is the selector and other is the value that you want to insert into text field
-    await Promise.all([page.click("#ourform button"),page.waitForNavigation()]) //we are using promise so to wait for the below to be executed
+    await page.type("#ourfield", "blue")
+    await Promise.all([page.click("#ourform button"),page.waitForNavigation()]) 
     const info= await page.$eval("#message",b =>b.textContent)
     console.log(info)
 
     for (const photo of photos) {
-        const imagepage=await page.goto(photo) //navigating through photo url
-        await fs.writeFile(photo.split("/").pop(),await imagepage.buffer())   //turning string of text into array, splitting
+        const imagepage=await page.goto(photo) 
+        await fs.writeFile(photo.split("/").pop(),await imagepage.buffer()) 
 
         
 
